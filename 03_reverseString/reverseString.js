942ae78a41ee3aed0145a48ca3fda7160856bf3a
Brian <talbertech@gmail.com>
2022-11-05 19:09:32 -0400

    reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let reversedString = "";
+    for(i = text.length-1; i >= 0; i--){
+        reversedString = reversedString + text.charAt(i);
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
+//using array solution
+//const reverseString = function(string) {
+//    return string.split('').reverse().join('');
+//   };
