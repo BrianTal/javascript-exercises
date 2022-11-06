07390ffd60f5a521575434680914ef6c760d20e8
Brian <talbertech@gmail.com>
2022-11-06 13:41:05 -0500

    exercise 04 and 05

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function (...arg) {
+    let array = arg[0];
+    let updatedArray = [];
+    array.forEach((each) => {
+      if (!arg.includes(each)) {
+        updatedArray.push(each);
+      }
+    });
+    return updatedArray;
+  };
 
 // Do not edit below this line
 module.exports = removeFromArray;
